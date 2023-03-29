e319193671bd74032d9725cce979f9fef06887d2
Kiet <vantuankiet2207@gmail.com>
2023-03-30 04:31:32 +0700

    làm dc xíu tài khoản

---
+++ app/src/main/java/com/example/doanbanhoa/HoSoNguoiDungActivity.java
@@ -0,0 +1,14 @@
+package com.example.doanbanhoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoSoNguoiDungActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ho_so_nguoi_dung);
+    }
+}
--- app/src/main/java/com/example/doanbanhoa/TaiKhoanFragment.java
+++ app/src/main/java/com/example/doanbanhoa/TaiKhoanFragment.java
@@ -1,12 +1,18 @@
 package com.example.doanbanhoa;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,11 +22,28 @@ import android.view.ViewGroup;
 public class TaiKhoanFragment extends Fragment {
 
 
-
+    ListView lstoptaikhoan;
     public TaiKhoanFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        String[] options = {"Địa Chỉ", "Hồ Sơ Người Dùng","Theo Dõi Đơn Hàng","Lịch Sử Mua Hàng"};
+        lstoptaikhoan = view.findViewById(R.id.optiontaikhoan);
+        ArrayAdapter<String> op = new ArrayAdapter<>(getContext(),R.layout.layout_listview_itemtaikhoan,options);
+        lstoptaikhoan.setAdapter(op);
+        lstoptaikhoan.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0: break;
+                    case 1: startActivity(new Intent(getContext(),HoSoNguoiDungActivity.class));
+                }
+            }
+        });
+    }
 
     public static TaiKhoanFragment newInstance() {
         TaiKhoanFragment fragment = new TaiKhoanFragment();
