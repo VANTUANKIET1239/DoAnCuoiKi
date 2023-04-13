515524d13ea96fd06ee522b776a05cc6e623f0d4
Kiet <vantuankiet2207@gmail.com>
2023-04-13 19:24:47 +0700

    gg

--- app/src/main/java/com/example/doanbanhoa/Activity/DoiMatKhauActivity.java
+++ app/src/main/java/com/example/doanbanhoa/Activity/DoiMatKhauActivity.java
@@ -16,16 +16,19 @@ import android.widget.Toast;
 import com.example.doanbanhoa.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class DoiMatKhauActivity extends AppCompatActivity {
 
-    EditText txtemail, txtmkmoi,txtxnmkm;
+    EditText txtemail, txtmkcu, txtmkmoi, txtxnmkm;
 
     Button btndoi;
 
     FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,42 +39,51 @@ public class DoiMatKhauActivity extends AppCompatActivity {
         txtxnmkm = findViewById(R.id.matkhaumoixacnhan);
         auth = FirebaseAuth.getInstance();
         btndoi = findViewById(R.id.btndoimatkhau);
+        txtmkcu = findViewById(R.id.matkhaucu);
 
         txtemail.setEnabled(false);
         txtemail.setText(auth.getCurrentUser().getEmail());
         btndoi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(txtxnmkm.getText().toString().equals(txtmkmoi.getText().toString())){
-                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    String newPassword = txtmkmoi.getText().toString();
 
-                    user.updatePassword(newPassword)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(getApplicationContext(),"Đổi Mật Khẩu Thành Công",Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-                                        auth.signOut();
-                                        finish();
-                                        MainActivity.main.finish();
-                                    }
-                                    else {
-                                        Toast.makeText(getApplicationContext(),"Đổi Mật Khẩu Thất Bại",Toast.LENGTH_SHORT).show();
-                                    }
-                                }
+                if (txtxnmkm.getText().toString().equals(txtmkmoi.getText().toString())) {
+                     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-                            });
 
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"Mật khẩu mới xác nhận không khớp",Toast.LENGTH_SHORT).show();
+                    AuthCredential credential = EmailAuthProvider
+                            .getCredential(user.getEmail(), txtmkcu.getText().toString().trim());
+                    user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                user.updatePassword(txtmkmoi.getText().toString())
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(getApplicationContext(), "Đổi Mật Khẩu Thành Công", Toast.LENGTH_SHORT).show();
+                                                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                                    auth.signOut();
+                                                    finish();
+                                                    MainActivity.main.finish();
+                                                } else {
+                                                    Toast.makeText(getApplicationContext(), "Đổi Mật Khẩu Thất Bại", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+
+                                        });
+                            }
+                        }
+                    });
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Mật khẩu mới xác nhận không khớp", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
-
 
 
     }
